8b73a250efaf3b5699e10337e2056261d0f8523a
PooriyaSanaie <88898162+ELECEngineer@users.noreply.github.com>
2023-05-29 23:34:05 +0330

    Update simulator_handler.py

--- src/simulator_handler.py
+++ src/simulator_handler.py
@@ -5,10 +5,15 @@ import cv2
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import math
+from utils.vehicle_command import VehicleCommand
+import time
 
 
 class SimulatorHandler:
+
     def __init__(self, town_name):
+        self.radar_dataframe = None
         self.spawn_point = None
         self.vehicle = None
         self.rgb_cam_sensor = None
@@ -23,8 +28,8 @@ class SimulatorHandler:
 
         try:
             print("Trying to communicate with the client...")
-            client = carla.Client("localhost", 2000)
-            client.set_timeout(8.0)
+            client = carla.Client("127.0.0.1", 2000)
+            client.set_timeout(38.0)
             self.world = client.get_world()
             if os.path.basename(self.world.get_map().name) != town_name:
                 self.world: carla.World = client.load_world(town_name)
@@ -32,8 +37,8 @@ class SimulatorHandler:
             self.blueprint_library = self.world.get_blueprint_library()
             self.actor_list = []
             self.vehicle_list = []
-            self.IM_WIDTH = 800  # Ideally a config file should be defined for such parameters
-            self.IM_HEIGHT = 600
+            self.IM_WIDTH = 1024  # Ideally a config file should be defined for such parameters
+            self.IM_HEIGHT = 720
             print("Successfully connected to CARLA client")
         except Exception as error:
             raise Exception(f"Error while initializing the simulator: {error}")
@@ -44,7 +49,7 @@ class SimulatorHandler:
     def spawn_vehicle(self, spawn_index: int = 90):
         self.vehicle_blueprint = self.blueprint_library.filter("model3")[0]  # choosing the car
         self.spawn_point = self.world.get_map().get_spawn_points()[spawn_index]
-        self.vehicle = self.world.spawn_actor(self.vehicle_blueprint, self.spawn_point)
+        self.vehicle = self.world.spawn_actor( self.vehicle_blueprint, self.spawn_point)
         self.actor_list.append(self.vehicle)
 
     def set_weather(self, weather=carla.WeatherParameters.ClearNoon):
@@ -118,3 +123,48 @@ class SimulatorHandler:
         gnss_dict["altitude"] = gnss.altitude
         self.gnss_dataframe = self.gnss_dataframe.append(gnss_dict, ignore_index=True)
         self.gnss_dataframe.to_csv(os.path.join(self.save_dir, "gnss.csv"), index=False)
+
+    def lidar(self):
+        lidar_cam = None
+        lidar_bp = self.world.get_blueprint_library().find('sensor.lidar.ray_cast')
+        lidar_bp.set_attribute('channels', str(32))
+        lidar_bp.set_attribute('points_per_second', str(90000))
+        lidar_bp.set_attribute('rotation_frequency', str(40))
+        lidar_bp.set_attribute('range', str(20))
+        lidar_location = carla.Location(0, 0, 2)
+        lidar_rotation = carla.Rotation(0, 0, 0)
+        lidar_transform = carla.Transform(lidar_location, lidar_rotation)
+        lidar_sen = self.world.spawn_actor(lidar_bp, lidar_transform, attach_to=self.vehicle,
+                                           attachment_type=carla.AttachmentType.Rigid)
+        return lidar_sen
+
+    def radar(self):
+        rad_cam = None
+        rad_bp = self.world.get_blueprint_library().find('sensor.other.radar')
+        rad_bp.set_attribute('horizontal_fov', str(35))
+        rad_bp.set_attribute('vertical_fov', str(20))
+        rad_bp.set_attribute('range', str(20))
+        rad_location = carla.Location(x=2.0, z=1.0)
+        rad_rotation = carla.Rotation(pitch=5)
+        rad_transform = carla.Transform(rad_location, rad_rotation)
+        rad_ego = self.world.spawn_actor(rad_bp, rad_transform, attach_to=self.vehicle,
+                                         attachment_type=carla.AttachmentType.Rigid)
+        return rad_ego
+
+    def rad_callback(self, radar_data):
+        for index, detect in enumerate(radar_data):
+            radar_dict = {"index": index, "altitude": detect.altitude, "azimuth": detect.azimuth, "depth": detect.depth,
+                          "velocity": detect.velocity}
+        #     self.radar_dataframe = self.radar_dataframe.append(radar_dict, ignore_index=True)
+        #
+        # self.radar_dataframe.to_csv(os.path.join(self.save_dir, "radar.csv"), index=False)
+        return radar_dict
+
+    def colision(self):
+        collision_sensor = self.world.spawn_actor(self.blueprint_library.find('sensor.other.collision'),
+                                                  carla.Transform(), attach_to=self.vehicle,
+                                                  attachment_type=carla.AttachmentType.Rigid)
+        return collision_sensor
+
+    def colision_callback(self, event):
+        print(f"COLLISION: {event}")
