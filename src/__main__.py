476bd0e94b9d98e17933b95133c3be802d419be8
Alireza Eslami <70325860+alirezaeslami1374@users.noreply.github.com>
2023-06-07 20:11:46 +0330

    A2-Eslami-401882008

--- src/__main__.py
+++ src/__main__.py
@@ -4,16 +4,23 @@ import carla
 
 from src.simulator_handler import SimulatorHandler
 from utils.vehicle_command import VehicleCommand
+from path_following_handler import PathFollowingHandler
+import warnings
+
+warnings.filterwarnings("ignore")
 
 if __name__ == "__main__":
-    simulator_handler = SimulatorHandler(town_name="Town04")
-    simulator_handler.spawn_vehicle(spawn_index=13)
+    simulator_handler = SimulatorHandler(town_name="Town10HD_Opt")
+
     simulator_handler.set_weather(weather=carla.WeatherParameters.ClearNoon)
 
     # potential weather choices are [ClearNoon, ClearSunset, CloudyNoon, CloudySunset,
     # WetNoon, WetSunset, MidRainyNoon, MidRainSunset, HardRainNoon, HardRainSunset,
     # SoftRainNoon, SoftRainSunset]
-
+    path_following_handler = PathFollowingHandler(client=simulator_handler.client)
+    ego_spawn_point = path_following_handler.ego_spawn_point
+    ego_vehicle = simulator_handler.spawn_ego_vehicles(road_id=ego_spawn_point["road_id"],
+                                                       filtered_points_index=ego_spawn_point["filtered_points_index"])
     # add sensors
     rgb_cam = simulator_handler.rgb_cam()
     gnss_sensor = simulator_handler.gnss()
@@ -23,8 +30,14 @@ if __name__ == "__main__":
     rgb_cam.listen(lambda image: simulator_handler.rgb_cam_callback(image))
     imu_sensor.listen(lambda imu: simulator_handler.imu_callback(imu))
     gnss_sensor.listen(lambda gnss: simulator_handler.gnss_callback(gnss))
-    VehicleCommand(throttle=1.0).send_control(simulator_handler.vehicle)
-    time.sleep(20.0)
 
 
+    if path_following_handler.debug_mode:
+        path_following_handler.start()
+    else:
+        ego_pid_controller = path_following_handler.pid_controller(ego_vehicle,
+                                                                   path_following_handler.pid_values_lateral,
+                                                                   path_following_handler.pid_values_longitudinal)
+        path_following_handler.vehicle_and_controller_inputs(ego_vehicle, ego_pid_controller)
+        path_following_handler.start()
 
