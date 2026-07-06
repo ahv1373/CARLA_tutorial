812c0365c73ce6111def9d6ea17c97330be0b52d
arman <adanehvash@gmail.com>
2026-07-06 19:29:11 +0330

    added lidar, radar and colission

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,22 @@
-import time
+import glob
+import os
+import sys
 import random
-import carla
+import time
 
+try:
+    sys.path.append(glob.glob(
+        r'F:\carla\WindowsNoEditor\PythonAPI\carla\dist\carla-0.9.8-py3.7-win-amd64.egg'
+    )[0])
+except IndexError:
+    raise RuntimeError("Couldn't find the CARLA egg.")
+
+import carla
 from src.simulator_handler import SimulatorHandler
 from src.utils.vehicle_command import VehicleCommand
 
 class SimulationHyperParameters:
-    def __init__(self, simulation_time: float = 30, town_name: str = "Town10HD_Opt",
+    def __init__(self, simulation_time: float = 30, town_name: str = "Town01",
                  ego_spawn_index: int = random.randint(0, 30), weather: carla.WeatherParameters = carla.WeatherParameters.ClearNoon,
                  use_autopilot: bool = True):  # Town choice are Mine_01 and Town10HD_Opt
         self.simulation_time = simulation_time
@@ -23,6 +33,7 @@ if __name__ == "__main__":
     # Simulation hyperparameters
     sim_parameters = SimulationHyperParameters()
 
+
     simulator_handler = SimulatorHandler(town_name=sim_parameters.town_name)
     simulator_handler.spawn_vehicle(spawn_index=sim_parameters.ego_spawn_index)
     simulator_handler.set_weather(weather=sim_parameters.weather)
@@ -32,8 +43,15 @@ if __name__ == "__main__":
     instance_segmentation_cam = simulator_handler.instance_segmentation_cam()
     gnss_sensor = simulator_handler.gnss()
     imu_sensor = simulator_handler.imu()
+    lidar_sensor = simulator_handler.lidar()
+    radar_sensor = simulator_handler.radar()
+    collision_sensor = simulator_handler.collision()
+
 
     # listen to sensor data
+    lidar_sensor.listen(lambda data: simulator_handler.lidar_callback(data))
+    radar_sensor.listen(lambda data: simulator_handler.radar_callback(data))
+    collision_sensor.listen(lambda event: simulator_handler.collision_callback(event))
     rgb_cam.listen(lambda image: simulator_handler.rgb_cam_callback(image))
     instance_segmentation_cam.listen(lambda image: simulator_handler.instance_segmentation_callback(image))
     imu_sensor.listen(lambda imu: simulator_handler.imu_callback(imu))
@@ -58,4 +76,10 @@ if __name__ == "__main__":
     #         break
 
     time.sleep(sim_parameters.simulation_time)  # time.sleep is a blocking operation for letting the simulation run
-    simulator_handler.terminate()  # cleanup the script and remove actors
+    # at the very end of src/__main__.py
+    import os
+
+    simulator_handler.terminate()
+    print("Exiting.")
+    os._exit(0)   # hard-exit: skips interpreter teardown that triggers the fatal error
+
